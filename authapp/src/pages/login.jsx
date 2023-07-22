db91ceee0c500273f8fd3b27030dc3acfe944777
sha7hid <shahidshafadshahad@gmail.com>
2023-07-22 12:11:45 +0530

    assets and tailwind

---
+++ authapp/src/pages/login.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Layout from "../../layout/layout";
+export default function Login() {
+  return (
+    <>
+    <Layout>
+    <Head>
+        <title>Login</title>{" "}
+      </Head>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </Layout>
+      
+    </>
+  );
+}
---
+++ authapp/src/pages/register.jsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+export default function Register() {
+    return (
+        <><Head><title>Register</title> </Head><div>
+            <h1>Register</h1>
+        </div></>
+    );
+}
